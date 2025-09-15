1b0ea53baffcac366ad74a03afbe5b5f0517ad2b
dallonorrea <65102612+dallonorrea@users.noreply.github.com>
2025-09-15 11:19:21 -0500

    Add files via upload

---
+++ app/pages/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">Bienvenido</h1>
+      <p>
+        Usa el menú de la izquierda para navegar por las opciones del
+        aplicativo.
+      </p>
+    </div>
+  );
+}
